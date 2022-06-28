8dae46c0e37332de9428597c3289e6179107b82c
savita kumari <sakumari@zynga.com>
2022-06-28 11:28:24 +0530

    revalidation for root path

--- pages/api/revalidate-posts.js
+++ pages/api/revalidate-posts.js
@@ -1,16 +1,10 @@
 // pages/api/revalidate.js
 
-import { currentLocale } from '../../lib/common';
-
 export default async function handler(req, res) {
-	// const locale = currentLocale(req);
 	// check for POST request
 	if (req.method !== 'POST') {
 		res.status(400).json({ error: 'Invalid HTTP method. Only POST requests are allowed.' });
 	}
-
-	// console.log('req.query.secret------- ', req.query.secret);
-	// console.log('process.env.REVALIDATE------- ', process.env.REVALIDATE);
 
 	// check for secret token
 	if (req.query.secret !== process.env.REVALIDATE) {
@@ -28,26 +22,22 @@ export default async function handler(req, res) {
 	try {
 		const slugToRevalidate = body.slugToRevalidate;
 
-		console.log('slugToRevalidate------- ', slugToRevalidate);
-
 		const locale = body.locale;
 
-		console.log('locale------- ', locale);
-
 		if (slugToRevalidate) {
-			console.log('if --- slugToRevalidate------- ');
 			let path = `/posts/${slugToRevalidate}`;
+			let rootPath = `/`;
 			if (locale !== 'en-US') {
+				rootPath = `/${locale}`;
 				path = `/${locale}/posts/${slugToRevalidate}`;
 			}
-			console.log('path  ', path);
+			await res.unstable_revalidate(`${rootPath}`);
 			await res.unstable_revalidate(`${path}`);
 			return res.json({ revalidated: true });
 		}
 	} catch (err) {
 		// If there was an error, Next.js will continue
 		// to show the last successfully generated page
-		console.log('CATCHHHHH');
 		return res.status(500).send('Error revalidating');
 	}
 }
