9a51941f45e9f4d20ba7cd45dd2ff338a5da1995
savita kumari <sakumari@zynga.com>
2022-06-28 11:19:37 +0530

    revalidation

--- pages/api/revalidate-posts.js
+++ pages/api/revalidate-posts.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
 				path = `/${locale}/posts/${slugToRevalidate}`;
 			}
 			console.log('path  ', path);
-			await res.unstable_revalidate(`/es/posts/${slugToRevalidate}`);
+			await res.unstable_revalidate(`${path}`);
 			return res.json({ revalidated: true });
 		}
 	} catch (err) {
